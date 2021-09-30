7df4f6dd8b4e0b2987f9a743be3d75a35fb24f29
Oleh Kozynets <oleh.kozynets@outlook.com>
2021-09-30 12:57:39 +0000

    Add FEE_PASSPHRASE to certificates.

--- src/overlay/PeerAuth.cpp
+++ src/overlay/PeerAuth.cpp
@@ -28,7 +28,9 @@ makeAuthCert(Application& app, Curve25519Public const& pub)
     cert.expiration = app.timeNow() + expirationLimit;
 
     auto hash = sha256(xdr::xdr_to_opaque(
-        app.getNetworkID(), ENVELOPE_TYPE_AUTH, cert.expiration, cert.pubkey));
+        app.getNetworkID(), ENVELOPE_TYPE_AUTH, cert.expiration, cert.pubkey,
+        app.getFeePoolID()));
+
     CLOG_DEBUG(Overlay, "PeerAuth signing cert hash: {}", hexAbbrev(hash));
     cert.sig = app.getConfig().NODE_SEED.sign(hash);
     return cert;
@@ -63,7 +65,8 @@ PeerAuth::verifyRemoteAuthCert(NodeID const& remoteNode, AuthCert const& cert)
         return false;
     }
     auto hash = sha256(xdr::xdr_to_opaque(
-        mApp.getNetworkID(), ENVELOPE_TYPE_AUTH, cert.expiration, cert.pubkey));
+        mApp.getNetworkID(), ENVELOPE_TYPE_AUTH, cert.expiration, cert.pubkey,
+        mApp.getFeePoolID()));
 
     CLOG_DEBUG(Overlay, "PeerAuth verifying cert hash: {}", hexAbbrev(hash));
     return PubKeyUtils::verifySig(remoteNode, cert.sig, hash);
