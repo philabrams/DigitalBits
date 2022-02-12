db8b6ca44b2821f6c2c5146fbb5a57c06f4aaf97
Oleh Kozynets <oleh.kozynets@outlook.com>
2022-02-12 01:55:11 +0000

    WIP: Fixed library order.

--- src/secrets/SecretsManagerImpl.cpp
+++ src/secrets/SecretsManagerImpl.cpp
@@ -1,5 +1,13 @@
 #include "secrets/SecretsManagerImpl.h"
 
+#include <aws/core/Aws.h>
+#include <aws/core/utils/logging/LogLevel.h>
+#include <aws/secretsmanager/SecretsManagerClient.h>
+#include <aws/secretsmanager/model/GetSecretValueRequest.h>
+#include <aws/secretsmanager/model/GetSecretValueResult.h>
+
+#include "util/Logging.h"
+
 namespace digitalbits 
 {
 std::unique_ptr<SecretsManager>
@@ -19,6 +27,30 @@ SecretsManagerImpl::SecretsManagerImpl(Application& app) : mApp(app)
 
 std::string SecretsManagerImpl::getSecret(const std::string&) const
 {
-    return "TopSecretInfo";
+    Aws::SDKOptions options;
+    options.loggingOptions.logLevel = Aws::Utils::Logging::LogLevel::Debug;
+    // jnitialize AWS API
+    Aws::InitAPI(options);
+    auto secretId = "tutorial/MyFirstSecret";
+    Aws::SecretsManager::SecretsManagerClient sm;
+    Aws::SecretsManager::Model::GetSecretValueRequest request;
+
+    request.SetSecretId(secretId);
+    auto outcome = sm.GetSecretValue(request);
+    if (outcome.IsSuccess())
+    {
+        CLOG_INFO(Ledger, "Secret retrieval successful.");
+        auto& result = outcome.GetResult();
+        CLOG_INFO(Ledger, "Secret name {}", result.GetName().c_str());
+        CLOG_INFO(Ledger, "Secret value {}", result.GetSecretString().c_str());
+    }
+    else
+    {
+        CLOG_INFO(Ledger, "Secret retrieval failed {}", outcome.GetError());
+    }
+
+    Aws::ShutdownAPI(options);
+
+    return "";
 }
 }
